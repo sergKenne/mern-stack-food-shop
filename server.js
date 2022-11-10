96f131b99328089332ee066b4b601b94222aed51
Serge Simplice Kenne <kenne_serge@mail.ru>
2022-11-10 03:16:34 +0300

    test commit

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const app = express();
 
 //if (process.env.NODE_ENV == 'production') {
     app.use(express.static('client/build'));
-    const path = require('path');
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
