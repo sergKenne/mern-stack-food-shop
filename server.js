57d35fed35256b91e6867e56979f6d2df2bc51dd
Serge Simplice Kenne <kenne_serge@mail.ru>
2022-11-10 21:48:39 +0300

    change host name

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ const path = require('path');
 const app = express();
 app.use(express.static(path.resolve(__dirname, 'client', 'build')));
     // app.use(express.static('client/build'));
-    // app.get('*', (req, res) => {
-    //     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    // });
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
 
 const PORT = process.env.PORT || 5000;
 
