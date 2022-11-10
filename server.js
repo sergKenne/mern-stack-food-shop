07e332f0a3431671978d4aab8635a5c2d69229fd
Serge Simplice Kenne <kenne_serge@mail.ru>
2022-11-10 22:21:46 +0300

    add cors

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 
 const express = require('express');
+const cors = require('cors');
 const path = require('path');
 const app = express();
+app.use(cors());
 app.use(express.static(path.resolve(__dirname, 'client', 'build')));
-    // app.use(express.static('client/build'));
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    });
+// app.use(express.static('client/build'));
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
 
 const PORT = process.env.PORT || 5000;
 
