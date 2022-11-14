087d6093c16ef2cc8599d2920d44997a6b903e9e
Serge Simplice Kenne <kenne_serge@mail.ru>
2022-11-14 05:43:33 +0300

    added Dockerfile

--- server.js
+++ server.js
@@ -5,9 +5,9 @@ const path = require('path');
 const app = express();
 app.use(cors());
 app.use(express.static('client/build'));
-// app.get('*', (req, res) => {
-//     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-// });
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
 
 const PORT = process.env.PORT || 5000;
 
