8599fea3d8cd90639416708db69ed6fee4520af1
Serge Simplice Kenne <kenne_serge@mail.ru>
2022-11-21 03:21:06 +0300

    test deploy

--- server.js
+++ server.js
@@ -6,9 +6,14 @@ const app = express();
 app.use(cors());
 //app.use(express.static('client/build'));
 //app.use(express.static(path.resolve(__dirname, 'client', 'build')));
+
+// app.get('*', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+// });
+
 app.use(express.static(path.resolve(__dirname, 'public')));
 app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
 });
 
 const PORT = process.env.PORT || 5000;
