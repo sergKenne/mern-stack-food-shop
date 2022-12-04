8683a972cbf3fccc986cf923b1a5ea6d475288ce
Serge Simplice Kenne <kenne_serge@mail.ru>
2022-12-04 14:38:07 +0300

    rebuild app

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(cors());
 
 app.use(express.static(path.resolve(__dirname, 'public')));
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
 });
 
